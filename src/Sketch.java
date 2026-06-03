90db544d46e254bf84cd9b9650ff67ae703dfa2e
directconnections <235324857+directconnections@users.noreply.github.com>
2026-06-02 22:55:48 -0400

    Falling Sand Particles

--- src/Sketch.java
+++ src/Sketch.java
@@ -1,5 +1,4 @@
 import processing.core.PApplet;
-import processing.event.KeyEvent;
 import processing.event.MouseEvent;
 
 /**
@@ -8,8 +7,11 @@ import processing.event.MouseEvent;
  */
 public class Sketch extends PApplet {
     public int brushRadius = 10;
-    public boolean pauseRender = false;
-    public byte[][] canvas;  // x then y
+    public short[][] canvas;  // x then y
+
+    public int canvasWidth = 60;
+    public int canvasHeight = 40;
+    public int sandSize = 5;
 
     public static void main(String[] args) {
         PApplet.main("Sketch");
@@ -17,60 +19,53 @@ public class Sketch extends PApplet {
 
     @Override
     public void settings() {
-        size(600, 400);
-
-        canvas = new byte[60][40];
+        size(canvasWidth * sandSize, canvasHeight * sandSize);
+        canvas = new short[canvasWidth][canvasHeight];
     }
 
     @Override
     public void setup() {
-        noStroke();
         fill(255);
     }
 
     @Override
     public void draw() {
         background(0);
-        if (pauseRender) {
-            textAlign(LEFT, TOP);
-            text("fps: " + frameRate, 0, 0);
-            return;
-        }
 
-        background(0);
-        scale(10);
-
-        for (int x = 0; x < canvas.length; x++) {
-            byte[] yAxis = canvas[x];
-
-            for (int y = 0; y < yAxis.length; y++) {
-                if (yAxis[y] == 1) {
-                    fill(255);
-                } else {
-                    fill(0);
-                }
-
-                rect(x, y, 10, 10);
+        
+        // render
+        noStroke();
+        for (int x = 0; x < canvasWidth; x++) {
+            for (int y = 0; y < canvasHeight; y++) {
+                fill((int)canvas[x][y]);
+                square(x * sandSize, y * sandSize, sandSize);
             }
         }
 
+        // update
+        for (int x = canvasWidth - 1; x >= 0; x--) {
+            for (int y = canvasHeight - 2; y >= 0; y--) {  // bottom particles do not need to keep falling
+                if (canvas[x][y] != 0 && canvas[x][y + 1] == 0) {
+                    canvas[x][y + 1] = canvas[x][y];
+                    canvas[x][y] = 0;
+                }
+            }
+        } 
+
+        
+        // fps
         fill(255);
-
-        resetMatrix();
-
         textAlign(LEFT, TOP);
-        text("fps: " + frameRate, 0, 0);
+        text(String.format("fps %d", (int)frameRate), 0, 0);
     }
 
     @Override
     public void mouseDragged(MouseEvent event) {
         // System.out.println(event.getX() + " " + event.getY());
-        canvas[event.getX() / 10][event.getY() / 10] = 1;
-    }
+        int x = Math.clamp(event.getX() / sandSize, 0, canvasWidth - 1);
+        int y = Math.clamp(event.getY() / sandSize, 0, canvasHeight - 1);
 
-    @Override
-    public void keyPressed(KeyEvent event) {
-        pauseRender = !pauseRender;
+        canvas[x][y] = 255;
     }
 
     /** Additional helper methods below */
